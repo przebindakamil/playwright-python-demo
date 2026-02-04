9a4be45b91bf5cb438d661220fad4eea7ee6c3d9
KamilPrzebinda <przebindakamil@gmail.com>
2026-02-04 14:59:55 +0100

    test: update pytest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,46 @@
+from dataclasses import dataclass
+
 import pytest
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Browser, Page, BrowserContext
+
+from pages.basic_cart_page import BasicCartPage
+from pages.seven_char_val_page import SevenCharValPage
+from pages.triangle_page import TrianglePage
 
 
 @pytest.fixture(scope="session")
-def browser():
+def browser() -> Browser:
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False)
         yield browser
         browser.close()
 
 
 @pytest.fixture
-def context(browser):
+def context(browser: Browser) -> BrowserContext:
     ctx = browser.new_context()
     yield ctx
     ctx.close()
 
 
 @pytest.fixture
-def page(context):
+def page(context: BrowserContext) -> Page:
     p = context.new_page()
     yield p
     p.close()
+
+
+@dataclass
+class AppPages:
+    triangle: TrianglePage
+    seven_char_val: SevenCharValPage
+    basic_cart: BasicCartPage
+
+
+@pytest.fixture
+def app_pages(page: Page) -> AppPages:
+    return AppPages(
+        triangle=TrianglePage(page),
+        seven_char_val=SevenCharValPage(page),
+        basic_cart=BasicCartPage(page),
+    )
